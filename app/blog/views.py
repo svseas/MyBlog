404aa6bdc3e1f7cd0251f2872a8e84e09f5787c6
Truong Tang <txt2408@gmail.com>
2023-06-01 16:23:12 +0700

    adjust urls

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,20 +1,27 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request,
                   'blog/post/detail.html',
